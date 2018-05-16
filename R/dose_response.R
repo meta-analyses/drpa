c71af98a757d16b06c3ae721c84757422822cc59
usr110 <m.aliabbas@gmail.com>
2018-05-16 17:12:58 +0100

    Add dose_response function's skeleton

---
+++ R/dose_response.R
@@ -0,0 +1,18 @@
+#' Dose response for Physical Acitivity
+#'
+#' Based on our meta-analysis, returns a response value for a specific cause or disease. \cr
+#' 1. Disease/Cause: Name of the disease/cause - from a specific list\cr
+#' 2. Type: Mortality or Incidence. \cr
+#'
+#' At the end, this function returns a response values with or without confidence interval
+#'
+#'
+#' @param cause All-Cause or disease name
+#' @param outcome_type Mortality or Incidence
+#' @param confidence_interval Boolean variable to determine whether confidence intervals are returned or not
+#' @export
+#'
+
+dose_response <- function (cause, outcome_type, confidence_interval){
+
+}
