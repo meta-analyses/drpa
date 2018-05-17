6f82f0af6cc74748427db4180057a43e181aa942
usr110 <m.aliabbas@gmail.com>
2018-05-17 15:01:59 +0100

    Add dose and check if cause is from a predefined list

--- R/dose_response.R
+++ R/dose_response.R
@@ -9,10 +9,60 @@
 #'
 #' @param cause All-Cause or disease name
 #' @param outcome_type Mortality or Incidence
-#' @param confidence_interval Boolean variable to determine whether confidence intervals are returned or not
+#' @param dose Dose (all-cause or disease)
+#' @param confidence_interval Boolean variable to determine whether confidence intervals are returned or not - default false
+#' @return response for a specific dose
 #' @export
 #'
 
-dose_response <- function (cause, outcome_type, confidence_interval){
+dose_response <- function (cause, outcome_type, dose, confidence_interval = F){
+
+  if (is.na(dose) || class(dose) != "numeric")
+    stop ('Please provide dose in numeric')
+
+  if (!cause %in% c('all-cause-mortality', 'breast-cancer', 'cardiovascular-disease',
+                   'colon-cancer', 'coronary-heart-disease', 'endometrial-cancer',
+                   'heart-failure', 'lung-cancer', 'stroke', 'total-cancer')){
+    stop('Unsupported cause/disease. Please select from \n
+         all-cause-mortality \n
+         breast-cancer\n
+         cardiovascular-disease \n
+         colon-cancer \n
+         coronary-heart-disease \n
+         endometrial-cancer \n
+         heart-failure \n
+         lung-cancer \n
+         stroke \n
+         total-cancer')
+
+  }
+
+  if (!outcome_type %in% c('mortality', 'incidence')){
+    stop('Unsupported outcome_type. Please select from \n
+         mortality \n
+         incidence')
+  }
+
+  if (cause == 'all-cause-mortality' && outcome_type == 'incidence')
+    stop('Incidence does not exist for all-cause-mortality')
+
+  fname <- paste(cause, outcome_type, sep = "-")
+
+  if (cause == 'all-cause-mortality')
+    fname <- cause
+
+  print(fname)
+
+  lookup_table <- read_csv(system.file("extdata", paste0(fname, ".csv"), package = "drpa", mustWork = TRUE))
+  print(summary(lookup_table))
+
+  return(lookup_table[which.min(abs(lookup_table$dose - dose)), "RR"])
+
+  # browser()
+
+  if ((!is.null(dose) && !is.na(dose)) && dose <= 35)
+    return(lookup_table[which.min(abs(lookup_table$dose - dose)), "RR"])
+  else
+    return(lookup_table[which.min(abs(lookup_table$dose - 35)), "RR"])
 
 }
