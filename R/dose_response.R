049856e9c4aa115f9861b702f02dfc8cc376d706
usr110 <m.aliabbas@gmail.com>
2021-06-04 16:37:32 +0100

    Update list of causes and outcome types

--- R/dose_response.R
+++ R/dose_response.R
@@ -1,17 +1,18 @@
-#' Dose response for Physical Acitivity
+#' Dose response for Physical Activity
 #'
 #' Based on our meta-analysis, returns a response value for a specific cause or disease. \cr
 #' 1. Disease/Cause: Name of the disease/cause - from a specific list\cr
-#' 2. Type: Mortality or Incidence. \cr
+#' 2. Type: Fatal, non-fatal or fatal-and-non-fatal. \cr
 #'
 #' At the end, this function returns a response values with or without confidence interval
 #'
 #'
-#' @param cause All-Cause or disease name
-#' @param outcome_type Mortality or Incidence
+#' @param cause All-Cause-mortality or disease name
+#' @param outcome_type Fatal, non-fatal or fatal-and-non-fatal
 #' @param dose Dose (all-cause or disease)
 #' @param confidence_intervals Boolean variable to determine whether confidence intervals are returned or not - default false
 #' @param certainty Certain response (fixed) or uncertain in the range of lower and upper bounds - default is certain (T)
+#' @param use_75_pert Last quantile determined by person years; beyond which there is less confidence
 #' @return response for a specific dose (in a data frame)
 #' @importFrom magrittr %>%
 #' @name %>%
@@ -24,15 +25,17 @@ dose_response <- function (cause, outcome_type, dose, confidence_intervals = F, 
   if (is.na(dose) || class(dose) != "numeric")
     stop ('Please provide dose in numeric')
 
-  if (!cause %in% c('all-cause-mortality', 'breast-cancer', 'cardiovascular-disease',
-                   'colon-cancer', 'coronary-heart-disease', 'diabetes', 'endometrial-cancer',
-                   'heart-failure', 'lung-cancer', 'stroke', 'total-cancer')){
+  if (!cause %in% c('all-cause-cancer', 'all-cause-cvd', 'all-cause-dementia', 'all-cause-mortality', 'bladder-cancer', 'breast-cancer', 'colon-cancer',
+                    'coronary-heart-disease', 'diabetes', 'endometrial-cancer', 'esophageal-cancer', 'gastric-cardia-cancer',
+                    'head-and-neck-cancer', 'heart-failure', 'kidney-cancer', 'liver-cancer', 'lung-cancer',
+                    'myeloma', 'prostate-cancer', 'rectum-cancer', 'stroke')){
     stop('Unsupported cause/disease. Please select from \n
          all-cause-mortality \n
          breast-cancer\n
          cardiovascular-disease \n
          colon-cancer \n
          coronary-heart-disease \n
+         diabetes \n
          endometrial-cancer \n
          heart-failure \n
          lung-cancer \n
@@ -41,21 +44,30 @@ dose_response <- function (cause, outcome_type, dose, confidence_intervals = F, 
 
   }
 
-  if (!outcome_type %in% c('mortality', 'incidence')){
+  if (!outcome_type %in% c('fatal', 'non-fatal', 'fatal-and-non-fatal')){
     stop('Unsupported outcome_type. Please select from \n
-         mortality \n
-         incidence')
+         fatal \n
+         non-fatal \n
+         fatal-and-non-fatal')
   }
 
-  if (cause == 'all-cause-mortality' && outcome_type == 'incidence')
-    stop('Incidence does not exist for all-cause-mortality')
+  if (cause == 'all-cause-mortality' && outcome_type == 'non-fatal')
+    stop('Non-fatal does not exist for all-cause-mortality')
+
+  if (cause == 'diabetes' && outcome_type != 'fatal')
+    stop('Non-fatal (and combined) does not exist for dementia')
 
   fname <- paste(cause, outcome_type, sep = "-")
 
-  if (cause == 'all-cause-mortality')
-    fname <- cause
+  #if (cause == 'all-cause-mortality')
+  #  fname <- cause
 
-  #print(fname)
+  print(fname)
+
+  if (!file.exists(system.file("extdata", paste0(fname, ".csv"),
+                              package = "drpa"))){
+    stop(paste('Sorry but for ', cause, ' the outcome type ', outcome_type, ' is not supported by the package'))
+  }
 
   lookup_table <- readr::read_csv(system.file("extdata", paste0(fname, ".csv"),
                                               package = "drpa",
@@ -72,6 +84,8 @@ dose_response <- function (cause, outcome_type, dose, confidence_intervals = F, 
   cond <- ifelse(use_75_pert,
                  abs(lookup_table$dose - dose),
                  which.min(abs(lookup_table$dose - dose)))
+
+
 
   if (confidence_intervals){
 
