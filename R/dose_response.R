ac2432acfe4f2a5172494c76706f86c199b2b839
Ali Abbas <m.aliabbas@gmail.com>
2021-09-01 14:35:17 +0100

    Update parameters and allow uncertainty using quantiles

--- R/dose_response.R
+++ R/dose_response.R
@@ -10,47 +10,58 @@
 #' @param cause All-cause-mortality or disease name
 #' @param outcome_type Fatal, non-fatal or fatal-and-non-fatal
 #' @param dose Dose (all-cause or disease)
-#' @param confidence_intervals Boolean variable to determine whether confidence intervals are returned or not - default false
-#' @param certainty Certain response (fixed) or uncertain in the range of lower and upper bounds - default is certain (T)
-#' @param use_75_pert Last quantile determined by person years; beyond which there is less confidence
+#' @param quantile Numeric value between 0 and 1 - default is 0.5
+#' @param use_75_pert Last quantile determined by person years; beyond which there is less confidence - default is TRUE
+#' @param confidence_intervals Boolean variable to determine whether confidence intervals are returned or not - default is FALSE
 #' @return response for a specific dose (in a data frame)
 #' @rdname dose_response
 #' @importFrom magrittr %>%
 #' @export
 #'
 
-dose_response <- function (cause, outcome_type, dose, confidence_intervals = F, certainty = T, use_75_pert = T){
+dose_response <- function (cause, outcome_type, dose, quantile = 0.5, use_75_pert = T, confidence_intervals = F){
 
   if (is.na(dose) || class(dose) != "numeric")
     stop ('Please provide dose in numeric')
 
-  if (!cause %in% c('all-cause-cancer', 'all-cause-cvd', 'all-cause-mortality', 'bladder-cancer', 'breast-cancer', 'colon-cancer',
-                    'coronary-heart-disease', 'diabetes', 'endometrial-cancer', 'esophageal-cancer', 'gastric-cardia-cancer',
-                    'head-and-neck-cancer', 'heart-failure', 'kidney-cancer', 'liver-cancer', 'lung-cancer',
-                    'myeloma', 'myeloid-leukemia', 'prostate-cancer', 'rectum-cancer', 'stroke')){
-    stop('Unsupported cause/disease. Please select from \n
-         all-cause-cancer \n
-         all-cause-mortality \n
-         all-cause-cvd \n
-         bladder-cancer \n
-         breast-cancer\n
-         colon-cancer \n
-         coronary-heart-disease \n
-         diabetes \n
-         endometrial-cancer \n
-         esophageal-cancer \n
-         gastric-cardia-cancer \n
-         head-and-neck-cancer \n
-         heart-failure \n
-         kidney-cancer \n
-         liver-cancer \n
-         lung-cancer \n
-         myeloma \n
-         myeloid-leukemia \n
-         prostate-cancer \n
-         rectum-cancer \n
-         stroke')
+  if (is.na(quantile) || class(quantile) != 'numeric' || quantile >= 1 || quantile < 0)
+    stop('Please provide quantile between 0 and less than 1')
 
+  pert_75 <- readr::read_csv(system.file("extdata", "75p_diseases.csv",
+                                         package = "drpa",
+                                         mustWork = TRUE),
+                             col_type = readr::cols())
+
+  if (!cause %in% pert_75$disease){
+    stop("Unsupported cause/disease. Please select from \n
+           all-cause-cancer  \n
+           all-cause-cvd  \n
+           all-cause-dementia \n
+           all-cause-mortality \n
+           alzheimer's-disease \n
+           bladder-cancer \n
+           breast-cancer \n
+           colon-cancer \n
+           coronary-heart-disease \n
+           depression \n
+           depressive-symptoms \n
+           diabetes \n
+           endometrial-cancer \n
+           esophageal-cancer \n
+           gastric-cardia-cancer \n
+           head-and-neck-cancer \n
+           heart-failure \n
+           kidney-cancer \n
+           liver-cancer \n
+           lung-cancer \n
+           major-depression \n
+           myeloid-leukemia \n
+           myeloma \n
+           parkinson's-disease \n
+           prostate-cancer \n
+           rectum-cancer \n
+           stroke \n
+           vascular-dementia \n")
   }
 
   if (!outcome_type %in% c('fatal', 'non-fatal', 'fatal-and-non-fatal')){
@@ -72,80 +83,40 @@ dose_response <- function (cause, outcome_type, dose, confidence_intervals = F, 
                                               mustWork = TRUE),
                                   col_type = readr::cols())
 
-  pert_75 <- readr::read_csv(system.file("extdata", "75p_diseases.csv",
-                                         package = "drpa",
-                                         mustWork = TRUE),
-                             col_type = readr::cols())
+  if(use_75_pert){
+    upper_limit <- pert_75 %>% dplyr::filter(disease == cause) %>% dplyr::select(outcome_type) %>% as.numeric()
+    dose[dose > upper_limit] <- upper_limit
+  }
 
-  # print(summary(pert_75))
+  rr <- approx(x = lookup_table$dose, y = lookup_table$RR, xout = dose,yleft = 1, yright = min(lookup_table$RR))$y
+  if (confidence_intervals || quantile != 0.5) {
+    lb <-
+      approx(
+        x = lookup_table$dose,
+        y = lookup_table$lb,
+        xout = dose,
+        yleft = 1,
+        yright = min(lookup_table$lb)
+      )$y
+    ub <-
+      approx(
+        x = lookup_table$dose,
+        y = lookup_table$ub,
+        xout = dose,
+        yleft = 1,
+        yright = min(lookup_table$ub)
+      )$y
+  }
+  ## we assume that the columns describe a normal distribution with standard deviation defined by the upper and lower bounds.
+  if (quantile != 0.5){
+    rr <- qnorm(quantile, mean = rr, sd = (ub-lb)/1.96)
+    rr[rr<0] <- 0
+  }
 
-  cond <- ifelse(use_75_pert,
-                 abs(lookup_table$dose - dose),
-                 which.min(abs(lookup_table$dose - dose)))
-
-
-
-  if (confidence_intervals){
-
-    if (certainty){
-
-
-      rr <- lookup_table[cond, "RR"] %>% as.numeric()
-      lb <- lookup_table[cond, "lb"] %>% as.numeric()
-      ub <- lookup_table[cond, "ub"] %>% as.numeric()
-
-      return (data.frame (rr = rr, lb = lb, ub = ub))
-
-    }else{
-
-
-      lb <- lookup_table[cond, "lb"] %>% as.numeric()
-      ub <- lookup_table[cond, "ub"] %>% as.numeric()
-      rr <- stats::runif(1, min=lb, max=ub)
-
-      return (data.frame (rr = rr, lb = lb, ub = ub))
-
-    }
-
+  if (confidence_intervals) {
+    return(data.frame (rr = rr, lb = lb, ub = ub))
   }else{
-
-
-    if (certainty){
-
-      rr = lookup_table[cond, "RR"] %>% as.numeric()
-      return(data.frame(rr = rr))
-
-    }else{
-
-
-      lb <- lookup_table[cond, "lb"] %>% as.numeric()
-      ub <- lookup_table[cond, "ub"] %>% as.numeric()
-
-      rr <- stats::runif(1, min=lb, max=ub)
-      return(data.frame(rr = rr))
-
-    }
-
+    return(data.frame(rr = rr))
   }
-
-  if (certainty){
-
-
-    lb <- lookup_table[cond, "lb"] %>% as.numeric()
-    ub <- lookup_table[cond, "ub"] %>% as.numeric()
-
-    return(stats::runif(1, min=lb, max=ub))
-  }
-  else{
-
-    return(lookup_table[cond, "RR"] %>% as.numeric())
-  }
-
-  # browser()
-
-  # if ((!is.null(dose) && !is.na(dose)) && dose <= 35)
-  #   return(lookup_table[which.min(abs(lookup_table$dose - dose)), "RR"])
-  # else
-  #   return(lookup_table[which.min(abs(lookup_table$dose - 35)), "RR"])
 
 }
